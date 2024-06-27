ec8694d29a9309f7c1db32e9959bccb904094d34
yamapyblack <s25050730r@gmail.com>
2024-06-27 16:51:39 +0900

    bugfix endtime

--- frontend/src/components/JoinChallenge.tsx
+++ frontend/src/components/JoinChallenge.tsx
@@ -1,55 +1,80 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { ConnectWallet }  from '../components/Button/ConnectWallet';
-import { useAccount, useWriteContract } from "wagmi";
+import { ConnectWallet } from "../components/Button/ConnectWallet";
+import { useAccount, useWriteContract, useReadContract } from "wagmi";
 import LoadingIndicator from "./LoadingIndicator";
-import { GmonsterAbi } from '../constants/GmonsterAbi';
-import { GmonsterAddress } from '../constants/GmonsterAddress';
+import { GmonsterAbi } from "../constants/GmonsterAbi";
+import { GmonsterAddress } from "../constants/GmonsterAddress";
 import { toast } from "sonner";
-import { parseEther } from 'viem';
+import { parseEther } from "viem";
 
 const JoinChallenge = () => {
-  const [startTime, setStartTime] = useState('07:00');
-  const [endTime, setEndTime] = useState('10:00');
-  const [timezone, setTimezone] = useState('');
-  const { isConnected } = useAccount();
+  const [startTime, setStartTime] = useState("07:00");
+  const [endTime, setEndTime] = useState("10:00");
+  const [timezone, setTimezone] = useState("");
+  const { isConnected, address: userAddress } = useAccount();
   const { isPending, writeContract } = useWriteContract();
-  const DEPOSIT_AMOUNT = parseEther('0.002');
-  
+  const DEPOSIT_AMOUNT = parseEther("0.002");
+  const [isDeposited, setIsDeposited] = useState(false);
+
+  const { data: challengesData } = useReadContract({
+    address: GmonsterAddress as `0x${string}`,
+    abi: GmonsterAbi,
+    functionName: "challenges",
+    args: [userAddress!],
+  });
 
   useEffect(() => {
     const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
     setTimezone(tz);
   }, []);
 
+  useEffect(() => {
+    if (challengesData) {
+      console.log("challengesData", challengesData);
+      if (challengesData[0] > 0) {
+        console.log("Deposited");
+        setIsDeposited(true);
+      }
+    }
+  }, [challengesData]);
+
   const handleStartTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newStartTime = e.target.value;
     setStartTime(newStartTime);
-    // Set endTime to 10:00 AM on July 4th, 2024 (GMT0)
-    const endDate = new Date(Date.UTC(2024, 6, 4, 10, 0)); // Note: Months are zero-indexed (6 is July)
-    setEndTime(endDate.toTimeString().slice(0, 5));
+    //Set end time to 3 hours later
+    const [hours, minutes] = newStartTime.split(":").map(Number);
+    const newMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const newHours = hours + 3 >= 24 ? hours + 3 - 24 : hours + 3;
+    const newEndTime = `${newHours}:${newMinutes}`;
+    setEndTime(newEndTime);
   };
 
   const getEndTimeUnixTimestamp = (): bigint => {
-    const [hours, minutes] = endTime.split(':').map(Number);
-    const endDate = new Date(2024, 6, 4, hours, minutes); // July 4th, 2024
-    
+    const [hours, minutes] = endTime.split(":").map(Number);
+    //TODO set correct date
+    const chllengeDate = new Date(Date.UTC(2024, 5, 28)); // Months are 0-indexed, so 4 represents May
+    const chllengeTimestamp = Math.floor(chllengeDate.getTime() / 1000);
+    console.log("chllengeTimestamp", chllengeTimestamp);
+
+    const localEndDate = new Date(2024, 5, 28, hours, minutes);
+    let localEndTimestamp = Math.floor(localEndDate.getTime() / 1000);
+    console.log("localEndTimestamp", localEndTimestamp);
+
     // If the end time is earlier than the current time, set it for tomorrow
-    const now = new Date();
-    if (endDate < now) {
-      endDate.setDate(endDate.getDate() + 1);
+    if (chllengeTimestamp > localEndTimestamp) {
+      localEndDate.setDate(localEndDate.getDate() + 1);
+      localEndTimestamp = Math.floor(localEndDate.getTime() / 1000);
+      console.log("localEndTimestamp", localEndTimestamp);
     }
 
-    console.log("@@@endDate=", endDate)
-    
-    return BigInt(Math.floor(endDate.getTime() / 1000)); // Convert to seconds and then to BigInt
+    return BigInt(localEndTimestamp);
   };
-  
 
   const deposit = async () => {
     const endTimeUnix = getEndTimeUnixTimestamp();
-    console.log("@@@endTimeUnix=", endTimeUnix)
+    console.log("@@@endTimeUnix=", endTimeUnix);
     writeContract(
       {
         address: GmonsterAddress as `0x${string}`,
@@ -75,6 +100,7 @@ const JoinChallenge = () => {
               },
             },
           });
+          setIsDeposited(true);
         },
       }
     );
@@ -97,22 +123,33 @@ const JoinChallenge = () => {
                 className="mr-2"
               />
               <span className="mx-2">to</span>
-              <div className="ml-2 py-2 px-4 rounded">
-                {endTime}
-              </div>
+              <div className="ml-2 py-2 px-4 rounded">{endTime}</div>
               <span className="ml-2">({timezone})</span>
             </div>
           </div>
           <p className="text-sm text-gray-600 mb-6">
             Make sure your wallet address connected to your Farcaster account.
           </p>
-          
-          {isConnected ? (
-            <Button onClick={deposit} className="w-full bg-blue-500 hover:bg-blue-600 text-white text-lg py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
+
+          {!isConnected ? (
+            <ConnectWallet />
+          ) : !isDeposited ? (
+            <Button
+              onClick={deposit}
+              className="w-full bg-blue-500 hover:bg-blue-600 text-white text-lg py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105"
+            >
               {isPending ? <LoadingIndicator /> : "Deposit 0.002ETH"}
             </Button>
           ) : (
-            <ConnectWallet />
+            <Button
+              onClick={() =>
+                window.open("https://warpcast.com/~/channel/gmonster")
+              }
+              className="w-full text-white text-lg py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105"
+              style={{ backgroundColor: "#8A63D2" }}
+            >
+              Follow channel on Farcaster
+            </Button>
           )}
         </div>
       </div>
