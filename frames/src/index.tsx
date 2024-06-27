a3ac1ca4ab6a21e24df7355e6153d043de565e93
LIB <libcreatesweb3@gmail.com>
2024-06-27 12:27:24 +0800

    Revert "Revert "Fix image path""

--- frames/src/index.tsx
+++ frames/src/index.tsx
@@ -12,7 +12,7 @@ export const app = new Frog({
 app.frame('/', (c) => {
   return c.res({
     action: '/finish',
-    image: '/title.png',
+    image: 'https://gmon-frames.vercel.app/title.png',
     intents: [
       <Button.Transaction target="/challenge">GM</Button.Transaction>,
     ]
@@ -22,7 +22,7 @@ app.frame('/', (c) => {
 app.frame('/finish', (c) => {
   const { transactionId } = c
   return c.res({
-    image: '/gmon/01.png', // TODO: Changed everyday
+    image: 'https://gmon-frames.vercel.app/gmon/01.png', // TODO: Changed everyday
   })
 })
 
