1fefa11986dd95b2a32243e3074ecf195f433dd7
libdefi <libcreatesweb3@gmail.com>
2024-06-28 22:57:12 +0800

    Fix header build error

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link"; // Import the Link component
 import { useAccount } from "wagmi";
 
 const truncatedText = (val: string, num: number): string => {
@@ -10,31 +11,31 @@ const Header = () => {
   const { address } = useAccount();
 
   return (
-    <footer className="bg-blue-600 text-white py-2">
+    <header className="bg-blue-600 text-white py-2">
       <div className="container mx-auto px-4 flex justify-between items-center">
         <div className="">
-          <a href="/">
+          <Link href="/">
             <Image
               src="/GMonster-logo.png"
-              alt="GMaster Logo"
+              alt="GMonster Logo"
               width={240}
               height={10}
               className="max-w-full h-auto"
             />
-          </a>
+          </Link>
         </div>
         <div className="flex justify-between items-center">
           {address && (
             <>
               <div className="mr-3">
-                <Image src="/wallet_icon.png" alt="" width={18} height={18} />
+                <Image src="/wallet_icon.png" alt="Wallet Icon" width={18} height={18} />
               </div>
               <div>{truncatedText(address, 12)}</div>
             </>
           )}
         </div>
       </div>
-    </footer>
+    </header>
   );
 };
 
